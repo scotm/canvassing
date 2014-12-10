a83850b8e7f37e3f4a090798c4306b2631081b9c
Scott Macdonald <scott.scotm@gmail.com>
2014-12-10 17:10:32 +0000

    Basic webdev process install - bower, grunt et al

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'js/libs/*.js', // All JS in the libs folder
+                    'js/global.js'  // This specific file
+                ],
+                dest: 'js/build/production.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+
+        sass: {
+            options: {
+                sourceMap: true,
+                includePaths: ['bower_components/foundation/scss']
+            },
+            dist: {
+                files: {
+                    'main.css': 'scss/main.scss'
+                }
+            }
+        }
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-sass');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'sass']);
+    grunt.registerTask('styles', ['sass']);
+};
