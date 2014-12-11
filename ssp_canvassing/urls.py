51032c511d530ee4464b4f6c8690e2c336309108
Scott Macdonald <scott.scotm@gmail.com>
2014-12-11 16:57:15 +0000

    Sorting out the urls.

--- ssp_canvassing/urls.py
+++ ssp_canvassing/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^base/', TemplateView.as_view(template_name='base.html'))
 )
