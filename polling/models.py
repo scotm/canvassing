9742551833b6749ede4c67225080be283910c1df
Scott Macdonald <scott.scotm@gmail.com>
2015-02-02 14:58:27 +0000

    Updating database models for polling tables.

--- polling/models.py
+++ polling/models.py
@@ -1,24 +1,41 @@
-from django.contrib.gis.db import models
 from django.db import models
 
-# Create your models here.
 from campaigns.models import Campaign
-
-
-class CanvassQuestionaire(models.Model):
-    campaign = models.ForeignKey(Campaign)
 
 
 class CanvassQuestion(models.Model):
     polling_question = models.CharField(max_length=200)
     ordering = models.IntegerField()
+    type = models.CharField(max_length=20, choices=(
+    ('True/False', 'binary'), ('Multiple-choice', 'choice'), ('Detailed Answer', 'answer')), default='binary')
 
 
-class CanvassChoice(models.Model):
+# Is it a multiple-choice question? If so, what choices do we have for this question?
+class CanvassChoicesAvailable(models.Model):
     question = models.ForeignKey(CanvassQuestion)
+    option = models.CharField(max_length=100)
+
+
+class CanvassResponse(models.Model):
+    contact = models.ForeignKey('core.Contact', null=True)
+    question = models.ForeignKey(CanvassQuestion)
+
+    class Meta:
+        abstract = True
+
+
+class CanvassChoice(CanvassResponse):
     choice = models.CharField(max_length=200)
 
 
-class CanvassLongAnswer(models.Model):
-    question = models.ForeignKey(CanvassQuestion)
+class CanvassTrueFalse(CanvassResponse):
+    choice = models.NullBooleanField()
+
+
+class CanvassLongAnswer(CanvassResponse):
     answer = models.TextField()
+
+
+class CanvassQuestionaire(models.Model):
+    campaign = models.ForeignKey(Campaign)
+    questions = models.ManyToManyField(CanvassQuestion)
