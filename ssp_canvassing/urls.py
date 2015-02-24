79ac09d71f4375d144dbbf4f94ee3f0037dd7824
Scott Macdonald <scott.scotm@gmail.com>
2015-02-24 12:58:53 +0000

    Adding contact viewing code, and splitting off the url mapping

--- ssp_canvassing/urls.py
+++ ssp_canvassing/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^base/', TemplateView.as_view(template_name='base.html'))
+    url(r'^base/', TemplateView.as_view(template_name='base.html')),
+    url(r'^contact/', include('core.urls')),
 )
