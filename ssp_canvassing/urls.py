5185f1160454cfd8260b3ee0c6c82da2b2efde2a
Scott Macdonald <scott.scotm@gmail.com>
2015-02-26 17:09:36 +0000

    Map JavaScript tidy-up, and fixing of toggleable markers.

--- ssp_canvassing/urls.py
+++ ssp_canvassing/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 from core.views import DomecileMapView, DomecileAddressView
+
 
 urlpatterns = patterns('',
                        # Examples:
@@ -12,7 +14,8 @@ urlpatterns = patterns('',
                        url(r'^base/', TemplateView.as_view(template_name='base.html')),
                        url(r'^contact/', include('core.urls')),
                        url(r'^ward/', include('leafleting.urls')),
-                       url(r'^ajax/get_domeciles$', DomecileMapView.as_view()),
-                       url(r'^ajax/get_addresses$', DomecileAddressView.as_view())
+                       url(r'^postcode/', include('postcode_locator.urls')),
+                       url(r'^ajax/get_domeciles$', DomecileMapView.as_view(), name='get_domeciles'),
+                       url(r'^ajax/get_addresses$', DomecileAddressView.as_view(), name='get_addresses')
                        # url(r'^ward/(?P<slug>[a-z\-_]+)', WardView.as_view()),
 )
