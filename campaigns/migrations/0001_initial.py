bc7d6f63a845d58f1a124c21ff9dccb96e1d56dd
Scott Macdonald <scott.scotm@gmail.com>
2015-03-02 16:59:46 +0000

    Replace migrations properly.

--- campaigns/migrations/0001_initial.py
+++ campaigns/migrations/0001_initial.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),
     ]
 
     operations = [
@@ -16,7 +15,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100)),
-                ('parent_campaign', models.ForeignKey(to='campaigns.Campaign', null=True, blank=True)),
+                ('date_added', models.DateField(auto_now_add=True)),
+                ('parent_campaign', models.ForeignKey(blank=True, to='campaigns.Campaign', null=True)),
             ],
             options={
             },
