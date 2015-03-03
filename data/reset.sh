72057c03e7ea0014be429e5b443f8f10e4854c25
Scott Macdonald <scott.scotm@gmail.com>
2015-03-03 16:30:21 +0000

    Revising the reset button.

--- data/reset.sh
+++ data/reset.sh
@@ -5,3 +5,4 @@ dropdb ssp_canvassing; createdb ssp_canvassing; psql ssp_canvassing -c "CREATE E
 ./manage.py load_wards data/Wards_GB_2014_Boundaries/WD_DEC_2014_GB_BFE.shp
 ./manage.py load_regions data/bdline_gb/Data/scotland_and_wales_const_region.shp data/bdline_gb/Data/scotland_and_wales_region_region.shp data/bdline_gb/Data/westminster_const_region.shp
 ./manage.py add_from_csv_dundee data/pp20_28-08-2014_MARKED-UP_1.csv
+osm2pgsql -s --cache-strategy sparse -d ssp_canvassing -U ssp_canvassing -W data/scotland-latest.osm.pbf
