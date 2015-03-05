1dce0f259f3de6822961cac11281614dcde4c6a2
Scott Macdonald <scott.scotm@gmail.com>
2015-03-05 14:54:29 +0000

    Minor idiot fixup.

--- leafleting/urls.py
+++ leafleting/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
                        url(r'^leafleting/ward/$', LeafletWardPicker.as_view(), name='leaflet_ward_list'),
                        url(r'^leafleting/$', LeafletHomepage.as_view(), name='leaflet_homepage'),
 
-                       url(r'^canvassing/why_canvass/$', TemplateView.as_view(), name='why_canvass'),
+                       url(r'^canvassing/why_canvass/$', TemplateView.as_view(template_name="why_canvass.html"), name='why_canvass'),
                        url(r'^canvassing/canvass_run_create', CanvassRunCreate.as_view(), name='canvass_run_create'),
                        url(r'^canvassing/ward/(?P<pk>[0-9]+)', CanvassingPicker.as_view(), name='canvass_ward_view'),
                        url(r'^canvassing/ward/$', CanvassWardPicker.as_view(), name='canvass_ward_list'),
