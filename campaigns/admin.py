58730331ea19f958717f0a951b46cdd79fd8d6a2
Scott Macdonald <scott.scotm@gmail.com>
2015-03-13 11:30:51 +0000

    Bugfix

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from campaigns.models import Campaign, DownloadFilei
+from campaigns.models import Campaign, DownloadFile, AssignedLeafletRun, PrintableCanvassingRun, Signature
 
 admin.site.register(Campaign)
 admin.site.register(DownloadFile)
+admin.site.register(AssignedLeafletRun)
+admin.site.register(PrintableCanvassingRun)
+admin.site.register(Signature)
