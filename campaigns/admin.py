7fc8e848e9c29d0b444b27f039209a25c71a66df
Scott Macdonald <scott.scotm@gmail.com>
2015-03-13 10:34:45 +0000

    Some anonymised sample data

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from campaigns.models import Campaign, DownloadFile
+from campaigns.models import Campaign, DownloadFilei
 
 admin.site.register(Campaign)
 admin.site.register(DownloadFile)
