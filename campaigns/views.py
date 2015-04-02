2fe8e258f97ecf73d1087656dec648d2652d7c6c
Scott Macdonald <scott.scotm@gmail.com>
2015-04-02 16:39:19 +0100

    Basic changes to the campaigns - won't work correctly

--- campaigns/views.py
+++ campaigns/views.py
@@ -1,11 +1,10 @@
-from django.views.generic.edit import FormMixin
+__author__ = 'scotm'
+from django.views.generic import ListView, DetailView
+from django_filters.views import FilterView
 
 from campaigns.forms import ContactFindForm
-from core.models import Contact
-
-
-__author__ = 'scotm'
-from django.views.generic import ListView
+from core.models import Contact, PoliticalParty
+from leafleting.models import CanvassRun
 
 
 class FindContactList(ListView):
@@ -18,11 +17,27 @@ class FindContactList(ListView):
         queryset = self.model.objects.none()
         if form.is_valid():
             queryset = Contact.objects.filter(first_name__istartswith=form.cleaned_data['first_name'],
-                                          surname__iexact=form.cleaned_data['last_name'],
-                                          domecile__postcode__iexact=form.cleaned_data['postcode'])
+                                              surname__iexact=form.cleaned_data['last_name'],
+                                              domecile__postcode__iexact=form.cleaned_data['postcode'])
         return queryset
 
     def get_context_data(self, **kwargs):
         kwargs['form'] = self.form(self.request.GET) if self.request.GET else self.form()
         return super(FindContactList, self).get_context_data(**kwargs)
 
+
+class CanvassDataInput(DetailView):
+    template_name = 'data_input.html'
+    model = CanvassRun
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({'parties': PoliticalParty.objects.all()})
+        return super(CanvassDataInput, self).get_context_data(**kwargs)
+
+
+class CanvassRunFind(FilterView):
+    template_name = 'canvassrun_find.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({'user_runs': CanvassRun.objects.filter(booked_by=self.request.user)})
+        return super(CanvassRunFind, self).get_context_data(**kwargs)
