356e3b7a37403f38d180eaf8db49ebd9489bc308
Scott Macdonald <scott.scotm@gmail.com>
2015-04-09 17:00:56 +0100

    Encapsulate all the wards and regions when making them active - uses klasses instead

--- core/management/commands/define_active_wards.py
+++ core/management/commands/define_active_wards.py
@@ -1,20 +1,29 @@
 from __future__ import print_function
+from django.db.models.loading import get_model
 
 __author__ = 'scotm'
 
 from django.core.management import BaseCommand
 
-from core.models import Ward, Contact
+from core.models import Contact
 
 
-class Command(BaseCommand):
-    help = "Makes wards active if they've got contacts in them"
-
-    def handle(self, *args, **options):
-        for i in Ward.objects.all():
+def make_active(klasses):
+    for klass in klasses:
+        klass = get_model("core", klass)
+        for i in klass.objects.all():
             i.active = False
             if Contact.objects.filter(domecile__postcode_point__point__within=i.geom).exists():
                 i.active = True
             if i.active:
                 print("%s is active" % unicode(i))
             i.save(update_fields=['active'])
+
+
+class Command(BaseCommand):
+    help = "Makes wards active if they've got contacts in them"
+
+    klasses = ['Ward', 'Region']
+
+    def handle(self, *args, **options):
+        make_active(self.klasses)
