ca365450b4edade798a5993dc14138d1376e71f3
Scott Macdonald <scott.scotm@gmail.com>
2015-12-09 17:09:35 +0000

    Prepping the petition form

--- campaigns/views.py
+++ campaigns/views.py
@@ -1,16 +1,12 @@
 from braces.views import LoginRequiredMixin
+from django.views.generic import ListView
 from json_views.views import JSONDataView
 
+from campaigns.forms import ContactFindForm
 from campaigns.models import Signature, Campaign
-
+from core.models import Contact
 
 __author__ = 'scotm'
-from django.views.generic import ListView, DetailView
-from django_filters.views import FilterView
-
-from campaigns.forms import ContactFindForm
-from core.models import Contact, PoliticalParty
-from leafleting.models import CanvassRun
 
 
 class FindContactList(ListView):
@@ -22,31 +18,14 @@ class FindContactList(ListView):
         form = ContactFindForm(self.request.GET)
         queryset = self.model.objects.none()
         if form.is_valid():
-            queryset = Contact.objects.filter(first_name__istartswith=form.cleaned_data['first_name'],
-                                              surname__iexact=form.cleaned_data['last_name'],
-                                              domecile__postcode__iexact=form.cleaned_data['postcode'])
+            queryset = self.model.objects.filter(first_name__istartswith=form.cleaned_data['first_name'],
+                                                 surname__iexact=form.cleaned_data['last_name'],
+                                                 domecile__postcode__iexact=form.cleaned_data['postcode'])
         return queryset
 
     def get_context_data(self, **kwargs):
         kwargs['form'] = self.form(self.request.GET) if self.request.GET else self.form()
         return super(FindContactList, self).get_context_data(**kwargs)
-
-
-class CanvassDataInput(DetailView):
-    template_name = 'data_input.html'
-    model = CanvassRun
-
-    def get_context_data(self, **kwargs):
-        kwargs.update({'parties': PoliticalParty.objects.all()})
-        return super(CanvassDataInput, self).get_context_data(**kwargs)
-
-
-# class CanvassRunFind(LoginRequiredMixin, FilterView):
-#     template_name = 'canvassrun_find.html'
-#
-#     def get_context_data(self, **kwargs):
-#         kwargs.update({'user_runs': CanvassRun.objects.filter(booked_by=self.request.user)})
-#         return super(CanvassRunFind, self).get_context_data(**kwargs)
 
 
 class SignPetition(LoginRequiredMixin, JSONDataView):
