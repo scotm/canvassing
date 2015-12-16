6820911d04f3cdead8e219e1b6a3f9373128e098
Scott Macdonald <scott.scotm@gmail.com>
2015-12-16 13:54:37 +0000

    Replace the urls.py patterns with lists

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,10 +1,11 @@
+from django.conf.urls import url
+
 from campaigns.views import FindContactList, SignPetition, GetPetition
 
 __author__ = 'scotm'
-from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-                       url(r'^signatures/$', FindContactList.as_view(), name='find_contact'),
-                       url(r'^signatures/get_signature$', GetPetition.as_view(), name='get_petition_ajax'),
-                       url(r'^signatures/ajax_sign$', SignPetition.as_view(), name='sign_petition_ajax'),
-)
+urlpatterns = [
+    url(r'^signatures/$', FindContactList.as_view(), name='find_contact'),
+    url(r'^signatures/get_signature$', GetPetition.as_view(), name='get_petition_ajax'),
+    url(r'^signatures/ajax_sign$', SignPetition.as_view(), name='sign_petition_ajax'),
+]
--- ssp_canvassing/urls.py
+++ ssp_canvassing/urls.py
@@ -1,28 +1,24 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from core.views import DomecileMapView, DomecileAddressView, HomepageView, TemplateView
 
+urlpatterns = [
+    url(r'^$', HomepageView.as_view(), name='homepage'),
+    url(r'^about_the_ssp/$', TemplateView.as_view(template_name='about_the_ssp.html'),
+        name='about_the_ssp'),
+    url(r'bugs_and_features', TemplateView.as_view(template_name='bugs.html'), name='bugs'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^base/', TemplateView.as_view(template_name='base.html')),
+    url(r'^contact/', include('core.urls')),
+    url(r'^', include('leafleting.urls')),
+    url(r'^', include('polling.urls')),
+    url(r'^postcode/', include('postcode_locator.urls')),
+    url(r'reports/', include('reporting.urls')),
+    url(r'', include('campaigns.urls')),
+    url(r'^ajax/get_domeciles$', DomecileMapView.as_view(), name='get_domeciles'),
+    url(r'^ajax/get_addresses$', DomecileAddressView.as_view(), name='get_addresses'),
 
-urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'ssp_canvassing.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-                       url(r'^$', HomepageView.as_view(), name='homepage'),
-                       url(r'^about_the_ssp/$', TemplateView.as_view(template_name='about_the_ssp.html'),
-                           name='about_the_ssp'),
-                       url(r'bugs_and_features', TemplateView.as_view(template_name='bugs.html'), name='bugs'),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^base/', TemplateView.as_view(template_name='base.html')),
-                       url(r'^contact/', include('core.urls')),
-                       url(r'^', include('leafleting.urls')),
-                       url(r'^', include('polling.urls')),
-                       url(r'^postcode/', include('postcode_locator.urls')),
-                       url(r'reports/', include('reporting.urls')),
-                       url(r'', include('campaigns.urls')),
-                       url(r'^ajax/get_domeciles$', DomecileMapView.as_view(), name='get_domeciles'),
-                       url(r'^ajax/get_addresses$', DomecileAddressView.as_view(), name='get_addresses'),
-
-                       url('^', include('django.contrib.auth.urls')),
-                       # url(r'^ward/(?P<slug>[a-z\-_]+)', WardView.as_view()),
-                       )
+    url('^', include('django.contrib.auth.urls')),
+    # url(r'^ward/(?P<slug>[a-z\-_]+)', WardView.as_view()),
+]
