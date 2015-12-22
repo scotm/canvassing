dc52aee183d0310c59169b98ff1bd94f63b0d460
Scott Macdonald <scott.scotm@gmail.com>
2015-12-22 12:39:28 +0000

    Separate out the get_address_only test and the contact->domecile relationship test

--- tests/test_core/test_models.py
+++ tests/test_core/test_models.py
@@ -1,8 +1,7 @@
 # Create your tests here.
-import mock as mock
 
 from core.models import Domecile
-from tests.factories import ContactFactory, DomecileFactory
+from tests.factories import ContactFactory
 from tests.testcase import LazyTestCase
 
 
@@ -11,16 +10,13 @@ class ModelsTest(LazyTestCase):
         self.contact = ContactFactory.create(domecile__address_2='33', domecile__postcode='DD2 9XE',
                                              domecile__postcode_point__postcode='DD29XE')
 
-
     def test_domecile_lists(self):
         d = Domecile.get_sorted_addresses(self.contact.domecile.postcode)
         ContactFactory.create_batch(5)
         self.assertEqual(d, Domecile.get_sorted_addresses(self.contact.domecile.postcode))
 
     def test_domecile(self):
-        self.assertIn("33, Snookit Street, Dundee", self.contact.domecile.get_address_only())
         self.assertTrue(self.contact in self.contact.domecile.get_contacts())
 
     def test_get_address_only(self):
-        print self.contact.domecile.get_address_only()
-
+        self.assertIn("33, Snookit Street, Dundee", self.contact.domecile.get_address_only())
